33e7ba1df62d480bd5d6f90495403373bf74bfb0
luis <l.a.espinozaolivares@gmail.com>
2026-02-21 17:01:54 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/* let precio = 400000;
+
+let precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio;
+
+let cantidadSpan = document.querySelector(".cantidad");
+let cantidad = parseInt(cantidadSpan.innerHTML);
+
+let suma = precio + cantidad; */
+
+
+
+
+
+
+
+
